1b86845026b26c97c021f2884ceb5fbd167a737e
Matthew Slocum <matthew@redteal.com>
2017-04-27 12:07:13 -0700

    updates

---
+++ gen_package.py
@@ -0,0 +1,39 @@
+import os
+from os import walk
+
+def load_classes():
+    if os.path.isdir('src/classes'):
+        files = []
+        for (dirpath, dirnames, filenames) in walk('src/classes'):
+            files.extend(filenames)
+            break
+        for f in files:
+            if not f.endswith('.cls'):
+                files.remove(f);
+    return files;
+
+
+
+# open file
+text_file = open("package.xml", "w")
+# write headers
+text_file.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+text_file.write('<Package xmlns="http://soap.sforce.com/2006/04/metadata">\n')
+# write classes
+classes = load_classes()
+print classes
+if len(classes) > 0:
+    text_file.write('\t<types>\n')
+    for c in classes:
+        text_file.write('\t\t<members>'+c[:-4]+'</members>\n')
+    text_file.write('\t\t<name>ApexClass</name>\n')
+    text_file.write('\t</types>\n')
+
+# close package
+text_file.write('\t<version>39.0</version>\n')
+text_file.write('</Package>\n')
+
+text_file.close()
+
+
+#text_file.write("Purchase Amount: %s" % TotalAmount)
