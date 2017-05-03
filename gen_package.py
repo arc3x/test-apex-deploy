a7b1e4a653d5c8a158af064d56c5428ed534c208
Matthew Slocum <matthew@redteal.com>
2017-05-03 12:30:02 -0700

    updated gen script to include static resources

--- gen_package.py
+++ gen_package.py
@@ -1,3 +1,5 @@
+#import zipfile
+import shutil
 import os
 from os import walk
 
@@ -23,6 +25,29 @@ def write_xml(file_in, name_in, list_in):
         file_in.write('\t\t<members>'+os.path.splitext(os.path.basename(l))[0]+'</members>\n')
     file_in.write('\t\t<name>'+name_in+'</name>\n')
     file_in.write('\t</types>\n')
+
+# zips a directory and moves the resulting resource file into a staticresources folder in the dest directory
+def zip_to_static_resource(dir_to_zip, zip_name, dest):
+    # verify file structure
+    if not os.path.isdir(dir_to_zip):
+        return 0
+    if not os.path.isdir(dest):
+        return 0
+    if dest.endswith('/'):
+        dest = dest[:-1]
+    # build full path
+    full_dest_path = dest + '/staticresources'
+    # creat staticresources folder if not existant
+    if os.path.isdir(dest) and not os.path.isdir(full_dest_path):
+        os.makedirs(full_dest_path)
+    # zip dir
+    # zipf = zipfile.ZipFile(name, 'w', zipfile.ZIP_DEFLATED)
+    # for root, dirs, files in os.walk(dir_to_zip):
+    #     for file in files:
+    #         zipf.write(os.path.join(root, file))
+    # zipf.close()
+    shutil.make_archive(zip_name, 'zip', dir_to_zip)
+    shutil.move(zip_name+'.zip', full_dest_path+'/'+zip_name+'.resource')
 
 
 
@@ -50,6 +75,11 @@ print 'triggers ' + str(triggers)
 pages = load('src/pages', '.page')
 write_xml(text_file, 'ApexPage', pages)
 print 'pages ' + str(pages)
+# build & write static resource
+zip_to_static_resource('app', 'app', 'src')
+resources = load('src/staticresources', '.resource')
+write_xml(text_file, 'StaticResource', resources)
+print 'resources ' + str(resources)
 # close package
 text_file.write('\t<version>39.0</version>\n')
 text_file.write('</Package>\n')
